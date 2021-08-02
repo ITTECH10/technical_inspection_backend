203af78695c6edf9c473d314168e4a6bc2a8b940
ITTECH10 <ittech.a1development@gmail.com>
2021-08-02 19:28:42 +0200

    get my data route

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,3 +10,16 @@ exports.getAllUsers = catchAsync(async(req, res, next) => {
         users
     })
 })
+
+exports.getMyCredentials = catchAsync(async(req, res, next) => {
+    const user = await User.findOne({_id: req.user._id})
+
+    if(!user) {
+        return next(new AppError('There was a problem loading your account data.', 404))
+    }
+
+    res.status(200).json({
+        message: 'success',
+        user
+    })
+})
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,12 +8,16 @@ router.route('/login')
 .post(authController.login)
 
 router.use(authController.protect)
+
+router.route('/me')
+.get(userController.getMyCredentials)
+
 router.use(authController.restrictTo('admin'))
 
 router.route('/signup')
 .post(authController.signup)
 
-// router.route('/')
-// .get(userController.getAllUsers)
+router.route('/')
+.get(userController.getAllUsers)
 
 module.exports = router
