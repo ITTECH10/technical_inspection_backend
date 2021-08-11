e166c5fd373c2024439f3ea2513aa5c48ec46e8c
ITTECH10 <ittech.a1development@gmail.com>
2021-08-11 20:21:05 +0200

    cloudinary prod

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -24,6 +24,32 @@ const AppError = require('./appError');
 //     req.files.joinedTemp = joinedTemp + filename
 // }
 
+// DEVELOPMENT
+// exports.uploadSingleImage = req => {
+//     const ext = req.files.photo.mimetype.split('/')[1];
+//     const filename = `user-${req.user._id}-${new Date().getTime() * 1000}.${ext}`;
+
+//     if (!req.files.photo.mimetype.startsWith('image')) {
+//         return next(new AppError('Invalid file type. Please upload only images.', 400))
+
+//     } else if (req.files.photo.mimetype !== 'image/jpeg' && req.files.photo.mimetype !== 'image/png') {
+//         return next(new AppError('Please provide an image type with jpg or png file extension.', 400));
+//     }
+
+//     let temp = req.files.photo.tempFilePath.split('\\')
+//     let index = temp.length - 1;
+//     temp.splice(index, 1, req.files.filename)
+//     let joinedTemp = temp.join('\\')
+
+//     req.files.photo.mv(joinedTemp + filename, (err) => {
+//         if (err) console.log(err)
+//     })
+
+//     req.files.filename = filename
+//     req.files.joinedTemp = joinedTemp + filename
+// }
+
+// PRODUCTION
 exports.uploadSingleImage = req => {
     const ext = req.files.photo.mimetype.split('/')[1];
     const filename = `user-${req.user._id}-${new Date().getTime() * 1000}.${ext}`;
@@ -35,10 +61,10 @@ exports.uploadSingleImage = req => {
         return next(new AppError('Please provide an image type with jpg or png file extension.', 400));
     }
 
-    let temp = req.files.photo.tempFilePath.split('\\')
+    let temp = req.files.photo.tempFilePath.split('/')
     let index = temp.length - 1;
     temp.splice(index, 1, req.files.filename)
-    let joinedTemp = temp.join('\\')
+    let joinedTemp = temp.join('/')
 
     req.files.photo.mv(joinedTemp + filename, (err) => {
         if (err) console.log(err)
