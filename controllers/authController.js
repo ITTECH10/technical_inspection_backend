977dadc1018b8396e484e6ab16d6a9934c72c497
ITTECH10 <ittech.a1development@gmail.com>
2021-08-29 14:54:12 +0200

    sameSite

--- controllers/authController.js
+++ controllers/authController.js
@@ -60,8 +60,6 @@ exports.protect = catchAsync(async (req, res, next) => {
         token = req.headers.authorization.split(' ')[1]
     }
 
-    console.log(req.cookies.jwt)
-
     if (req.cookies.jwt) {
         token = req.cookies.jwt
     }
