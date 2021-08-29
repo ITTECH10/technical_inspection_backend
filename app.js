e809690bd10259676e6b8d7739eeea1f5a9ec6a5
ITTECH10 <ittech.a1development@gmail.com>
2021-08-29 11:56:01 +0200

    fixing cors origin

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 
-// const origin = process.env.NODE_ENV === 'production' ? 'https://technical-inspection-frontend.vercel.app/' : 'http://localhost:3000'
-const origin = 'https://technical-inspection-frontend.vercel.app'
+const origin = process.env.NODE_ENV === 'production' ? 'https://technical-inspection-frontend.vercel.app' : 'http://localhost:3000'
 
 app.use(cors({ credentials: true, origin }));
 app.use(cookieParser())
