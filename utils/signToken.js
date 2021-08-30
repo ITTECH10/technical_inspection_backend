02461946915e55984629fc220ec0bc5f3bd9fb2e
ITTECH10 <ittech.a1development@gmail.com>
2021-08-30 10:01:12 +0200

    fixed typo

--- utils/signToken.js
+++ utils/signToken.js
@@ -13,12 +13,12 @@ const createSendToken = (user, statusCode, res) => {
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        sameSite: 'none'
         // secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
     }
 
     if (process.env.NODE_ENV === 'production') {
         cookieOptions.sameSite = 'none'
+        cookieOptions.secure = true
     }
 
     res.cookie('jwt', token, cookieOptions);
