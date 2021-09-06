2c683ec7e4039a3f848da050dcf8779af50db423
ITTECH10 <ittech.a1development@gmail.com>
2021-09-06 09:04:01 +0200

    fixing reset url

--- controllers/authController.js
+++ controllers/authController.js
@@ -118,10 +118,10 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     }
 
     const resetToken = user.createPasswordResetToken()
-    const resetURL = `${req.protocol}://localhost:3000/resetPassword/${resetToken}`;
+    // const resetURL = `${req.protocol}://localhost:3000/resetPassword/${resetToken}`;
 
     // FOR TESTING BELLOW
-    // const resetURL = `https://secarmanagement.vercel.app/resetPassword/${resetToken}`
+    const resetURL = `https://secarmanagement.vercel.app/resetPassword/${resetToken}`
 
     await user.save({ validateBeforeSave: false })
 
