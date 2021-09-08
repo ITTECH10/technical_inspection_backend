4e493c10c7a8a78507f8f05e5c4b736637ca7414
ITTECH10 <ittech.a1development@gmail.com>
2021-09-08 14:29:15 +0200

    improved api security

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const AppError = require('./utils/appError')
 const userRouter = require('./routers/userRouter')
 const carRouter = require('./routers/carRouter')
 const bankRouter = require('./routers/bankRouter')
-// const fileRouter = require('./routers/fileRouter')
 const paymentRouter = require('./routers/paymentRouter')
 const insuranceHouseRouter = require('./routers/insuranceHouseRouter')
 const globalErrorHandler = require('./controllers/errorController')
@@ -11,20 +10,32 @@ const cors = require('cors')
 const fileupload = require('express-fileupload')
 const os = require('os')
 const cookieParser = require('cookie-parser')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
 const app = express()
-
 const origin = process.env.NODE_ENV === 'production' ? 'https://secarmanagement.vercel.app' : 'http://localhost:3000'
+
+// SECURITY HEADERS
+app.use(helmet())
 
 app.use(cors({ credentials: true, origin }));
 app.use(cookieParser())
+
+// CONSIDER LIMITING REQ.BODY
 app.use(express.json())
+
+// DATA SANITIZATION
+app.use(mongoSanitize())
+app.use(xss())
 
 app.use(fileupload({
     useTempFiles: true,
     tempFileDir: os.tmpdir()
 }));
 
+// TEST MIDLEWARE
 // app.use((req, res, next) => {
 //     console.log(req.cookies)
 //     next()
